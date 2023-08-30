bfa1598766e9f224b305bd9152310578023d3e29
RileyManda <rileymanda69@gmail.com>
2023-08-30 11:06:49 +0200

    Added comments

--- decoding_morse.rb
+++ decoding_morse.rb
@@ -1,6 +1,9 @@
+# Create a method to decode a Morse code character
 def decode_char(morse_code)
   morse_code_dict = {
-    ".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D",
+    ".-" => "A",
+    "-..." => "B",
+    "-.-." => "C", "-.." => "D",
     "." => "E", "..-." => "F", "--." => "G", "...." => "H",
     ".." => "I", ".---" => "J", "-.-" => "K", ".-.." => "L",
     "--" => "M", "-." => "N", "---" => "O", ".--." => "P",
@@ -9,5 +12,22 @@ def decode_char(morse_code)
     "-.--" => "Y", "--.." => "Z"
   }
 
-  morse_code_dict[morse_code.strip.gsub(/\s+/, '')] || "UNKNOWN"
+  morse_code_dict[morse_code.strip] || "UNKNOWN"
 end
+
+
+
+
+# Create a method to decode an entire word in Morse code
+def decode_word(morse_code)
+  morse_code.split(" ").map { |morse_code_char| decode_char(morse_code_char) }.join("")
+end
+
+
+# Create a method to decode a Morse code character
+
+
+# Create a method to decode the entire message in Morse code
+
+
+# Create a method that prints hellow world in morse code
